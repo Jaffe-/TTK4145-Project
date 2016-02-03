6872674562c44bee2b1421c760d557974d84d2e5
Duvarahan Kirishnathasan <duva_92@msn.com>
2016-02-03 10:12:22 +0100

    changed from TCP to UDP

--- network/receiver.cpp
+++ network/receiver.cpp
@@ -15,7 +15,7 @@ Receiver::Receiver(std::string port)
   int rv;
   memset(&hints, 0, sizeof(hints));
   hints.ai_family = AF_INET;
-  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_socktype = SOCK_DGRAM;
   hints.ai_flags = AI_PASSIVE;
 
   if ((rv = getaddrinfo(NULL, port.c_str(), &hints, &res)) != 0) {
@@ -26,11 +26,6 @@ Receiver::Receiver(std::string port)
   if ((sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) == -1){
     //fail!!
   }
-  
-  int yes = 1;
-  if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
-    //fail
-  } 
 
   if (bind(sockfd, res->ai_addr, res->ai_addrlen) == -1) {
     close(sockfd);
