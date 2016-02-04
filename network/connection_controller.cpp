16f59f6871217fa6efdb09feca297e4c123c651b
Duvarahan Kirishnathasan <duva_92@msn.com>
2016-02-04 13:29:29 +0100

    implement connectioncontroller

--- network/connection_controller.cpp
+++ network/connection_controller.cpp
@@ -1,11 +1,44 @@
 #include "connection_controller.hpp"
+#include <vector>
+#include "../logger/logger.hpp"
+
+double ConnectionController::get_time()
+{
+  return static_cast<double>(std::clock())/CLOCKS_PER_SEC;
+}
 
 void ConnectionController::notify_pong(std::string ip) 
 {
-  connections[ip] = time(NULL);
+  connections[ip] = get_time();
+}
+
+void ConnectionController::check_timeouts()
+{
+  std::vector<std::string> timed_out;
+  for (auto& kv : connections) {
+    double difference = get_time() - kv.second;
+    if (difference > timeout_limit) {
+      LOG_WARNING(kv.first << " stopped responding to PING");
+      // notify the callback thing
+      timed_out.push_back(kv.first);
+    }
+  }
+  for (auto& ip : timed_out) {
+    connections.erase(ip);
+  }
+}
+
+void ConnectionController::send_ping()
+{
+  double time = get_time();
+  if (time - last_ping >= ping_period) {
+    // send ping
+    last_ping = time; 
+  }
 }
 
 void ConnectionController::run()
 {
-
+  check_timeouts();
+  send_ping();
 }
--- network/connection_controller.hpp
+++ network/connection_controller.hpp
@@ -7,5 +7,11 @@ public:
   void notify_pong(std::string ip);
   void run();
 private:
-  std::map<std::string, time_t> connections;
-}
+  const double timeout_limit = 0.5;
+  const double ping_period = 0.1;
+  double last_ping;
+  std::map<std::string, double> connections;
+  double get_time();
+  void check_timeouts();
+  void send_ping();
+};
