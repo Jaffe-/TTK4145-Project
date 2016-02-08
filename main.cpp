8d09dd3ff4734c188b0634f95ad2f8cd6ad4c50d
Johan Fjeldtvedt <jaffe1@gmail.com>
2016-02-09 00:13:54 +0100

    add cmd options to main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
+#include "cmdopts/cmdopts.hpp"
 #include "network/receiver.hpp"
 
-int main()
+int main(int argc, char** argv)
 {
+  CmdOptions cmd_options;
+  if (!cmd_options_get(cmd_options, argc, argv,
+		       {{"port", 1, "<port>", "The port used for sending and receiving"}})) {
+    return -1;
+  }
+
   Network::start("30021");
   Network::receiver->run();
 }
