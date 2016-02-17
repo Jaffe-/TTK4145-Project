0e19f18d8197eaee79ed4adf81676185285970b2
Johan Fjeldtvedt <jaffe1@gmail.com>
2016-02-17 23:49:29 +0100

    use shared_ptr instead of unique: gives the ability to share the same message instead of copy or manual MM

--- msg_testing.cpp
+++ msg_testing.cpp
@@ -24,6 +24,8 @@ public:
   T data;
 };
 
+/* Wraps a handler function taking Message<T> in a lambda which takes care of
+   converting the message to the correct Message<T> type. */
 template <typename T>
 std::function<void(const IMessage&)> wrap(void (*handler)(const Message<T>&))
 {
@@ -42,36 +44,22 @@ void handler2(const Message<int>& s)
   std::cout << "INT: " << s.data << std::endl;
 }
 
-/* Helpful in C++11 */
-template <typename T>
-std::unique_ptr<T> make_unique_ptr(T* ptr)
-{
-  return std::unique_ptr<T>(ptr);
-}
-
 int main()
 {
-  std::queue<std::unique_ptr<IMessage>> Q;
+  std::queue<std::shared_ptr<IMessage>> Q;
 
   std::map<TMessage, std::function<void(const IMessage&)>> handlers = {
     {TMessage::NETWORK_SEND, wrap(handler1)},
     {TMessage::WHATEVER, wrap(handler2)}
   };
 
-  /* C++14:
-  Q.push(std::make_unique<Message<std::string>>(TMessage::NETWORK_SEND, "HEI HEI"));
-  Q.push(std::make_unique<Message<int>>(TMessage::WHATEVER, 1234));
-  Q.push(std::make_unique<Message<std::string>>(TMessage::NETWORK_SEND, "hoho"));
-  Q.push(std::make_unique<Message<std::string>>(TMessage::NETWORK_SEND, "hals"));
-  */
-
-  Q.push(make_unique_ptr(new Message<std::string>(TMessage::NETWORK_SEND, "HEI HEI")));
-  Q.push(make_unique_ptr(new Message<int>(TMessage::WHATEVER, 1234)));
-  Q.push(make_unique_ptr(new Message<std::string>(TMessage::NETWORK_SEND, "hoho")));
-  Q.push(make_unique_ptr(new Message<std::string>(TMessage::NETWORK_SEND, "hals")));
+  Q.push(std::make_shared<Message<std::string>>(TMessage::NETWORK_SEND, "HEI HEI"));
+  Q.push(std::make_shared<Message<int>>(TMessage::WHATEVER, 1234));
+  Q.push(std::make_shared<Message<std::string>>(TMessage::NETWORK_SEND, "hoho"));
+  Q.push(std::make_shared<Message<std::string>>(TMessage::NETWORK_SEND, "hals"));
 
   while (!Q.empty()) {
-    std::unique_ptr<IMessage> msg {std::move(Q.front())};
+    std::shared_ptr<IMessage> msg {std::move(Q.front())};
     handlers[msg->type](*msg);
     Q.pop();
   }
