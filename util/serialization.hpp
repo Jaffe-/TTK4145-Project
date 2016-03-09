6290ec3e0df82ea9e7ca459bbe65a2e3d4dcc1f8
Duvarahan Kirishnathasan <duva_92@msn.com>
2016-03-09 12:19:46 +0100

    added serialization interface

---
+++ util/serialization.hpp
@@ -0,0 +1,12 @@
+#include "json.hpp"
+
+using json_t = nlohmann::json;
+
+class Serializable {
+public:
+  virtual json_t get_json() const = 0;
+  virtual void deserialize(const std::string& s) = 0;
+  std::string serialize() {
+    return get_json().dump();
+  }
+};
