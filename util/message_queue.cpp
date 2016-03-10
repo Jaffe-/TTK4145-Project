17a048fc54eda35831c55af757959a3bceb51e17
Johan Fjeldtvedt <jaffe1@gmail.com>
2016-03-10 20:41:37 +0100

    add message handling capabilities to message_queue

--- util/message_queue.cpp
+++ util/message_queue.cpp
@@ -3,13 +3,11 @@
 #include <cassert>
 
 
-/* Acquire immediately locks the queue */
 std::unique_lock<std::mutex> MessageQueue::acquire()
 {
   return std::unique_lock<std::mutex>(mut);
 }
 
-/* Wait will suspend the thread until a new message has arrived */
 std::unique_lock<std::mutex> MessageQueue::wait()
 {
   std::unique_lock<std::mutex> lock(mut);
@@ -17,23 +15,19 @@ std::unique_lock<std::mutex> MessageQueue::wait()
   return lock;
 }
 
-
-/* Push uses acquire() to get the lock and moves its argument into the queue */
-void MessageQueue::push(const std::shared_ptr<Message>& msg)
-{
-  {
-    auto lock = acquire();
-    queue.push_back(std::move(msg));
-  }
-  new_message.notify_one();
-}
-
-
-/* Use the given lock to take all current messages (move them out of the queue) */ 
 MessageQueue::queue_t MessageQueue::take_messages(std::unique_lock<std::mutex> lock)
 {
   assert(lock && lock.mutex() == &mut);
   auto messages = std::move(queue);
   queue = queue_t();
   return messages;
+}
+
+void MessageQueue::handle_messages(const queue_t& queue)
+{
+  for (const auto& msg : queue) {
+    if (handlers.find(typeid(*msg)) != handlers.end()) {
+      handlers[typeid(*msg)](*msg);
+    }
+  }
 }
--- util/message_queue.hpp
+++ util/message_queue.hpp
@@ -6,20 +6,17 @@
 #include <iostream>
 #include "serialization.hpp"
 #include <typeinfo>
+#include <typeindex>
+#include <unordered_map>
 
-/* This should contain all fields common to all kinds of messages. */
+
 class Message {
 public:
   virtual bool serializable() const {
     return false;
   };
 
-  /*
-    This operator allows a BaseMessage object to be automatically converted
-    to a Message<T>, where T depends on the context - i.e. when left hand side
-    in an assignment is a specific Message<T> or when a BaseMessage reference/
-    pointer is given to a function taking a specific Message<T>.
-  */
+  /* Used to cast a Message to its concrete derived type */
   template <typename T>
   operator const T&() const {
     if (typeid(T) == typeid(*this))
@@ -27,26 +24,58 @@ public:
     else throw std::bad_cast();
   }
 
-  /* Virtualizing the Serializable& conversion operator allows one to serialize
-     a message without knowing the type of the data in it. */
+  /* Used to cast itself to Serializable. Must be dynamic cast since
+     Message doesn't itself derive from Serializable. */
   operator const Serializable&() const {
     return dynamic_cast<const Serializable&>(*this);
   };
 };
 
+
 class MessageQueue {
 public:
   using queue_t = std::deque<std::shared_ptr<const Message>>;
+  using map_t = std::unordered_map<std::type_index, std::function<void(const Message&)>>;
 
+  /* Acquire immediately locks the queue */
   std::unique_lock<std::mutex> wait();
+
+  /* Wait will suspend the thread until a new message has arrived */
   std::unique_lock<std::mutex> acquire();
 
-  void push(const std::shared_ptr<Message>& msg);
+  /* Push any given message into the queue. The message object must be copy
+     constructible and derive from Message */
+  template <typename T>
+  void push(const T& msg) {
+    {
+      auto lock = acquire();
+      queue.push_back(std::make_shared<T>(msg));
+    }
+    new_message.notify_one();
+  }
 
+  /* Use the given lock to take all current messages (move them out of the queue) */
   queue_t take_messages(std::unique_lock<std::mutex> lock);
+
+
+  /* Add message handler function */
+  template <typename T>
+  void add_handler(std::function<void(const T&)> handler) {
+    handlers[typeid(T)] = handler;
+  }
+
+  /* Call the right message handlers for the messages in the given queue */
+  void handle_messages(const queue_t& queue);
+
+  /* A practical overload */
+  void handle_messages(std::unique_lock<std::mutex> lock) {
+    handle_messages(take_messages(std::move(lock)));
+  }
 
 private:
   queue_t queue;
   std::mutex mut;
   std::condition_variable new_message;
+
+  map_t handlers;
 };
