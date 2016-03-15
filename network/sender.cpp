2615c2ca3f82c8ef6f71e070b0b465a8f58b00f1
Johan Fjeldtvedt <jaffe1@gmail.com>
2016-03-15 11:49:29 +0100

    remove useless print in sender

--- network/sender.cpp
+++ network/sender.cpp
@@ -70,7 +70,6 @@ Packet Sender::make_packet(const std::string& msg, unsigned int id)
 void Sender::run()
 {
   for (auto& kv : network.connections) {
-    LOG_DEBUG("ip = " << kv.first);
     const std::string& ip = kv.first;
     std::vector<MessageEntry>& message_queue = kv.second.message_queue;
     if (!message_queue.empty()) {
