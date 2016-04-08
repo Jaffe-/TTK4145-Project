262819df714617d9c77e1ccd212533e7af1ece49
Johan Fjeldtvedt <jaffe1@gmail.com>
2016-04-08 11:36:51 +0200

    add comments to logic.cpp

--- logic/logic.cpp
+++ logic/logic.cpp
@@ -23,6 +23,9 @@ Logic::Logic(bool use_simulator, const std::string& port)
   event_queue.add_handler<LostNetworkEvent>(this, &Logic::notify);
 }
 
+/* Calculate the cost function (using the simulated FSM) for each elevator and
+   determine if this elevator should take the given order. If we have the
+   minimum value, we should take the order even though others may have it. */
 void Logic::choose_elevator(Button button)
 {
   int min = INT_MAX;
@@ -47,30 +50,41 @@ void Logic::choose_elevator(Button button)
   }
 }
 
+/* When an external button event occurs, it is broadcasted to the other
+   connections, and the choose function is run to determine whether this
+   elevator should take the order */
 void Logic::notify(const ExternalButtonEvent& event)
 {
   network.event_queue.push(event);
   choose_elevator(event.button);
 }
 
+/* When a state update event occurs, our own state in elevator_states should be
+   updated, and it should be broadcasted to the other elevators. */
 void Logic::notify(const StateUpdateEvent& event)
 {
   elevator_states["me"] = event.state;
   network.event_queue.push(event);
 }
 
+/* When a state update event is received from the network, it should be stored
+   in elevator_states, creating a new index if needed. */
 void Logic::notify(const NetworkReceiveEvent<StateUpdateEvent>& event)
 {
   LOG_DEBUG("Received " << event);
   elevator_states[event.ip] = event.data.state;
 }
 
+/* When an external button event is received from the network, the choose
+   function is run to determine whether this elevator should take the order. */
 void Logic::notify(const NetworkReceiveEvent<ExternalButtonEvent>& event)
 {
   LOG_DEBUG("Received " << event);
   choose_elevator(event.data.button);
 }
 
+/* When a connection is lost, that elevator's state should be removed from
+   elevator_states, since it should no longer be part of the decision making */
 void Logic::notify(const LostConnectionEvent& event)
 {
   auto it = elevator_states.find(event.ip);
@@ -78,17 +92,18 @@ void Logic::notify(const LostConnectionEvent& event)
     elevator_states.erase(it);
 }
 
+/* When a new elevator appears we must send our latest state to it. */
 void Logic::notify(const NewConnectionEvent&)
 {
   StateUpdateEvent state(elevator_states["me"]);
   network.event_queue.push(state);
 }
 
+/* When our own network connection is lost, remove all elevator states
+   except our own, as the information about the rest of the elevators
+   will be outdated. */
 void Logic::notify(const LostNetworkEvent&)
 {
-  /* When our own network connection is lost, remove all elevator states
-     except our own, as the information about the rest of the elevators
-     will be outdated. */
   for (auto it = elevator_states.begin(); it != elevator_states.end(); ) {
     if (it->first != "me")
       it = elevator_states.erase(it);
