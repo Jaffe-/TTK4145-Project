46c64d8d09a9aa220218cc649a4323dcae62549a
Johan Fjeldtvedt <jaffe1@gmail.com>
2016-04-09 16:38:53 +0200

    fixed bug where the calculate function returns a lower cost than it should

--- logic/fsm.cpp
+++ logic/fsm.cpp
@@ -4,7 +4,8 @@ int SimulatedFSM::calculate(int floor, int type)
 {
   insert_order(floor, type);
   int step = 0;
-  while (state.current_floor != floor) {
+  while (!(state.current_floor == floor &&
+	   type == static_cast<int>(state.direction))) {
     if (should_stop(state.current_floor))
       step++;
     if (state.direction == Direction::UP) {
