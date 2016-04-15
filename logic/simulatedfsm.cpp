b56fb2d6db59669e73a79a7f4207ebd10390755a
Johan Fjeldtvedt <jaffe1@gmail.com>
2016-04-15 18:34:22 +0200

    only check should_stop if step > 0, to avoid case where elevator is technically on same floor

--- logic/simulatedfsm.cpp
+++ logic/simulatedfsm.cpp
@@ -9,7 +9,7 @@ int SimulatedFSM::calculate(int floor, int type)
     LOG(5, "dir=" << (int)state.direction << " floor=" << state.current_floor
 	<< "state=" << (int)state.state_id);
     if (state.state_id == MOVING) {
-      if (should_stop(state.current_floor)) {
+      if (step > 0 && should_stop(state.current_floor)) {
 	state.state_id = STOPPED;
 	step++;
 	continue;
